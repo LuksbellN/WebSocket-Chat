4faed90b8d94014597afcefda2e394596e2a135b
LuksbellN <108474778+LuksbellN@users.noreply.github.com>
2022-09-05 16:16:29 -0300

    Uploading the project

---
+++ public/main.js
@@ -0,0 +1,109 @@
+const socket = io();
+let username = '';
+let userList = [];
+
+let loginPage = document.querySelector('#loginPage');
+let chatPage = document.querySelector('#chat');
+
+let loginInput = document.querySelector('#loginNameInput');
+let textInput = document.querySelector("#chatTextInput");
+
+loginPage.style.display = 'flex';
+chatPage.style.display = 'none';
+
+const renderUserList = () => {
+    let ul = document.querySelector('.userList');
+    ul.innerHTML = "<li>Online users</li>";
+    for(let i of userList) {
+        ul.innerHTML += `<li>${i}</li>`
+    }
+}
+
+const addMessage = (type, user, msg) => {
+    let ul = document.querySelector('.chatList');
+
+    switch(type) {
+        case 'status':
+            ul.innerHTML += '<li class="m-status">'+msg+'</li>'
+        break;
+        case 'msg':
+            if(username == user) {
+                ul.innerHTML += '<li class="m-txt"><span class="me">'+user+'</span> '+msg+'</li>'
+            } else {
+                ul.innerHTML += '<li class="m-txt"><span>'+user+'</span> '+msg+'</li>'
+            }
+        break;
+    }
+
+    ul.scrollTop = ul.scrollHeight;
+}
+
+loginInput.addEventListener('keyup', (e) => {
+    if(e.keyCode === 13) {
+        let name = loginInput.value.trim();
+        if(name != '') {
+            username = name;
+            document.title = 'Chat('+username+')'
+
+            socket.emit('join-request', username);
+        }
+    }
+})
+
+textInput.addEventListener('keyup', (e) => {
+    if(e.keyCode === 13) {
+        let txt = textInput.value.trim();
+        textInput.value = ""
+        if(txt != "") {
+            socket.emit('message-req', {
+                type: 'msg', 
+                user: username,
+                msg: txt
+            });
+            addMessage('msg', username, txt);
+        }
+    }
+})
+
+socket.on('userOk', (list) => {
+    loginPage.style.display = 'none';
+    chatPage.style.display = 'flex';
+
+    addMessage('status', null, 'Conectado!')
+
+    userList = list;
+    renderUserList();
+});
+
+socket.on('list-update', (data) => {
+    if(data.joined) {
+        addMessage('status', null, data.joined+' entrou no chat.')
+    }
+    if(data.left) {
+        addMessage('status', null, data.left+' saiu do chat.')
+    }
+    userList = data.list;
+    renderUserList();
+});
+
+socket.on('message', (data) => {
+    console.log(data)
+    addMessage(data.type, data.user, data.msg);
+});
+
+socket.on('disconnect', () => {
+    addMessage('status', null, "Você foi desconectado");
+    userList = [];
+    renderUserList();
+});
+
+socket.on('connect_error', () => {
+    addMessage('status', null, "Tentando reconectar...");
+});
+
+socket.io.on('reconnect', () => {
+    addMessage('status', null, 'Reconectado')
+    if(username != ''){
+      socket.emit('join-request', username)
+    }
+  })
---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server)
+
+server.listen(3000);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+let connectedUsers = [];
+
+io.on('connection', (socket) => {
+    console.log("Conexão detectada...");
+
+    socket.on('join-request', (username) => {
+        socket.username = username;
+        connectedUsers.push(username);
+        console.log(connectedUsers);
+        socket.emit('userOk', connectedUsers);
+        socket.broadcast.emit('list-update', {
+            joined: username,
+            list: connectedUsers
+        })
+    });
+
+    socket.on('disconnect', () => {
+        connectedUsers = connectedUsers.filter(u => u != socket.username);
+        console.log(connectedUsers);
+
+        socket.broadcast.emit('list-update', {
+            left: socket.username,
+            list: connectedUsers
+        })
+    })
+
+    socket.on('message-req', (data) => {
+        console.log(data)
+        let obj = {
+            type: data.type,
+            user: data.user,
+            msg: data.msg
+        }
+        
+        socket.broadcast.emit('message', obj)
+    })
+
+});
